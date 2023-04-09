46148975aa44fdb1a9b5c0f54f95e3405b132571
Daniele Passacantilli <daniele.passacantilli96@gmail.com>
2023-04-09 15:56:05 +0200

    refactor

--- src/utils/Const.java
+++ src/utils/Const.java
@@ -1,6 +1,6 @@
 package utils;
 
-public class Const {
+public abstract class Const {
     public static final String baseSpritePath = "res/sprites";
     public static final String bombermanPath = baseSpritePath + "/bomberman.png";
 }
--- src/utils/LogUtils.java
+++ src/utils/LogUtils.java
@@ -3,7 +3,7 @@ package utils;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class LogUtils {
+public abstract class LogUtils {
     public static String getCurrentDateString() {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
